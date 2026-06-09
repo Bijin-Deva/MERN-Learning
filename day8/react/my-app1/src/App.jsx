3d1426e57318e8675ac33dc8dca819bb52e1afb6
Bijin Deva Reddy <bijindeva1@gmail.com>
2026-06-09 15:44:46 +0530

    Worked with inline CSS

--- day8/react/my-app1/src/App.jsx
+++ day8/react/my-app1/src/App.jsx
@@ -1,38 +1,21 @@
 import './App.css';
 
-function StudentCard({name, course, city}){
+function StateMessage({isActive})
+{
   return(
-    <div style={{border: '1px solid #ccc',padding:'16px',borderRadius:'8px',margin:'8px',width:'150px', boxShadow:'0 4px 12px red'}}>
-      <h3>Name: {name}</h3>
-      <p><strong>Course: </strong>{course}</p>
-      <p><strong>City: </strong>{city}</p>
-    </div>
+    <h2 style={{ color: isActive?"green":"red" }}>
+      {isActive ? "User is Active" : "User is Inactive"}
+    </h2>
   )
 }
 
 
 
 function App(){
-  // const students = ["Ravi","Kishore","Srjana","sneha"];
-
-
-
   return(
     <div>
-      {/* <h2>Student List</h2>
-      <ul>
-        {
-          students.map((student,index)=>(
-            <li key={index}>{student}</li>
-          ))
-        }
-      </ul> */}
-      <h1>My Class Room</h1>
-      <StudentCard name="Arjun" course="MERN Stack" city="Kurnool"/>
-      <StudentCard name="Priya" course="CSS" city="Hyderabad"/>
-      <StudentCard name="Kiran" course="HTML" city="Chennai"/>
-      
-      
+      <StateMessage isActive={true} />
+      <StateMessage isActive={false} />
     </div>
   )
 }
