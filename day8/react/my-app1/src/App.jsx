9789f54665ca84d9c524d9df39eb72d02b313090
Bijin Deva Reddy <bijindeva1@gmail.com>
2026-06-09 12:46:05 +0530

    React Program initialization

---
+++ day8/react/my-app1/src/App.jsx
@@ -0,0 +1,14 @@
+function App(){
+  const studentName = "Arjun";
+  const course = "MERN Stack";
+  const year = 2025;
+  return(
+    <div>
+      <h1>Student Profile:</h1>
+      <p>Name: {studentName}</p>
+      <p>Course: {course}</p>
+      <p>Year: {year}</p>
+    </div>
+  )
+}
+export default App;
