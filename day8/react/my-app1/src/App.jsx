23fabcf39ee02cd7fc406f9040b7f729e937ade7
Bijin Deva Reddy <bijindeva1@gmail.com>
2026-06-09 15:14:14 +0530

    Usage of Props

--- day8/react/my-app1/src/App.jsx
+++ day8/react/my-app1/src/App.jsx
@@ -1,13 +1,38 @@
+import './App.css';
+
+function StudentCard({name, course, city}){
+  return(
+    <div style={{border: '1px solid #ccc',padding:'16px',borderRadius:'8px',margin:'8px',width:'150px', boxShadow:'0 4px 12px red'}}>
+      <h3>Name: {name}</h3>
+      <p><strong>Course: </strong>{course}</p>
+      <p><strong>City: </strong>{city}</p>
+    </div>
+  )
+}
+
+
+
 function App(){
-  const studentName = "Arjun";
-  const course = "MERN Stack";
-  const year = 2025;
+  // const students = ["Ravi","Kishore","Srjana","sneha"];
+
+
+
   return(
     <div>
-      <h1>Student Profile:</h1>
-      <p>Name: {studentName}</p>
-      <p>Course: {course}</p>
-      <p>Year: {year}</p>
+      {/* <h2>Student List</h2>
+      <ul>
+        {
+          students.map((student,index)=>(
+            <li key={index}>{student}</li>
+          ))
+        }
+      </ul> */}
+      <h1>My Class Room</h1>
+      <StudentCard name="Arjun" course="MERN Stack" city="Kurnool"/>
+      <StudentCard name="Priya" course="CSS" city="Hyderabad"/>
+      <StudentCard name="Kiran" course="HTML" city="Chennai"/>
+      
+      
     </div>
   )
 }
