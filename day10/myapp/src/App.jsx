90ecd592369ad3712306e1f55eaca71f96346b67
Bijin Deva Reddy <bijindeva1@gmail.com>
2026-06-12 11:56:45 +0530

    Worked with bootstrap classes

---
+++ day10/myapp/src/App.jsx
@@ -0,0 +1,269 @@
+function App(){
+    return(
+
+
+            <div className="container mt-5">
+                <dic className="row">
+                    <div className="col-md-5">
+                        <h2>Contact US</h2>
+                        <p className="text-muted">
+                            Send your Query. Our team will contact you soon...
+                        </p>
+                        <img src="/images/jeep.jpg" height="200" width="350" />
+                    </div>
+                    <div className="col-md-7">
+                        <div className="card shadow">
+                            <div className="card-body">
+                                <input className="form-control mb-3" placeholder="Your Name" />
+                                <input className="form-control mb-3" placeholder="Your Email" />
+                                <textarea className="form-control mb-3" rows="4" placeholder="Write your message here" />
+                                <button className="btn btn-primary">Send Message</button>
+     
+                            </div>
+                        </div>
+                    </div>
+                </dic>
+            </div>
+
+
+            // <div className="container-fluid">
+            //     <div className="row min-vh-100">
+            //         <div className="col-md-3 col-lg-2 bg-dark text-white p-4">
+              //             <hr />
+            //             <p>Dashboard</p>
+            //             <p>Students</p>
+            //             <p>Courses</p>
+            //             <p>Reports</p>
+            //             <p>Settings</p>
+            //         </div>
+
+            //         <div className="col-md-9 col-lg-10 p-4">
+            //             <h2>Dashboard</h2>
+            //             <p>Welcome to the admin dashboard</p>
+            //             <div className="container mt-4">
+            //     <h2 className="mb-4">Admin Dashboard</h2>
+            //     <div className="row">
+            //         <div className="col-md-3">
+            //             <div className="card bg-primary text-white shadow">
+            //                 <div className="card-body">
+            //                     <h5>Total Students</h5>
+            //                     <h2>245</h2>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //         <div className="col-md-3">
+            //             <div className="card bg-success text-white shadow">
+            //                 <div className="card-body">
+            //                     <h5>Registered Students</h5>
+            //                     <h2>174</h2>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //         <div className="col-md-3">
+            //             <div className="card bg-warning text-white shadow">
+            //                 <div className="card-body">
+            //                     <h5>Pending</h5>
+            //                     <h2>50</h2>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //         <div className="col-md-3">
+            //             <div className="card bg-danger text-white shadow">
+            //                 <div className="card-body">
+            //                     <h5>Not Verified</h5>
+            //                     <h2>23</h2>
+            //                 </div>
+            //             </div>
+            //         </div>
+            //         <div className="col-md-3 mt-5">
+            //             <img src="/images/jeep.jpg" />
+            //         </div>
+            //     </div>
+            // </div>
+            //         </div>
+            //     </div>
+            // </div>
+
+        // <div className="container mt-4">
+        //     <h2 className="mb-4">Admin Dashboard</h2>
+        //     <div className="row">
+        //         <div className="col-md-3">
+        //             <div className="card bg-primary text-white shadow">
+        //                 <div className="card-body">
+        //                     <h5>Total Students</h5>
+        //                     <h2>245</h2>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div className="col-md-3">
+        //             <div className="card bg-success text-white shadow">
+        //                 <div className="card-body">
+        //                     <h5>Registered Students</h5>
+        //                     <h2>174</h2>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div className="col-md-3">
+        //             <div className="card bg-warning text-white shadow">
+        //                 <div className="card-body">
+        //                     <h5>Pending</h5>
+        //                     <h2>50</h2>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //         <div className="col-md-3">
+        //             <div className="card bg-danger text-white shadow">
+        //                 <div className="card-body">
+        //                     <h5>Not Verified</h5>
+        //                     <h2>23</h2>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
+
+        // <div className="container mt-5">
+        //     <div className="row justify-content-center">
+        //         <div className="col-md-6">
+        //             <div className="card shadow">
+        //                 <div className="card-body">
+        //                     <h3 className="text-center mb-4">Student Registration Form</h3>
+
+        //                     <input type="text" className="form-control mb-3" placeholder="Full Name" />
+        //                     <input type="number" className="form-control mb-3" placeholder="Enter Mobile" />
+        //                     <input type="email" className="form-control mb-3" placeholder="Enter Email" />
+
+        //                     <select className="form-select mb-3">
+        //                         <option>Select Course</option>
+        //                         <option>MERN</option>
+        //                         <option>CSS</option>
+        //                         <option>React</option>    
+        //                     </select> 
+
+        //                     <button className="btn btn-success w-100">Register</button>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        //     <hr />
+        // </div>
+
+        // <div className="container mt-4 text-center">
+        //     <h2>Loading data....</h2>
+
+        //     <div className="spinner-border text-primary">
+        //         <span className="visually-hidden">Loading</span>
+        //     </div>
+        //     <hr />
+        // </div>
+
+        // <div className="container mt-4">
+        //     <nav>
+        //         <ul className="pagination">
+        //             <li className="page-item disabled">
+        //                 <a className="page-link">Previous</a>
+        //             </li>
+        //             <li className="page-item active">
+        //                 <a className="page-link">1</a>
+        //             </li>
+        //             <li className="page-item ">
+        //                 <a className="page-link">2</a>
+        //             </li>
+        //             <li className="page-item ">
+        //                 <a className="page-link">Next</a>
+        //             </li>
+        //         </ul>
+        //     </nav>
+        //     <hr />
+        // </div>
+
+        // <div className="container mt-4">
+        //     <nav>
+        //         <ol className="breadcrumb">
+        //             <li className="breadcrumb-item">Home</li>
+        //             <li className="breadcrumb-item">Student</li>
+        //             <li className="breadcrumb-item">Add Student</li>
+        //         </ol>
+        //     </nav>
+        //      <hr />
+        // </div>
+
+        // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        //     <div className="container">
+        //         <a className="navbar-brand" href="#">College App</a>
+
+        //         <div className="navbar-nav">
+        //             <a className="nav-link active" href="#">Dashboard</a>
+        //             <a className="nav-link" href="#">Student</a>
+        //             <a className="nav-link" href="#">Reports</a>
+        //         </div>
+        //     </div>
+        //     <hr />
+        // </nav>
+
+            // <div class="container">
+            //     <h2>Student Details</h2>
+            //     <table className="table table-bordered table-stripped">
+            //         <thead className="table-dark">
+            //             <tr>
+            //                 <th>ID</th>
+            //                 <th>Name</th>
+            //                 <th>Course</th>
+            //                 <th>Actions</th>
+            //             </tr>
+            //         </thead>
+            //         <tbody>
+            //             <tr>
+            //                 <td>1</td>
+            //                 <td>Bijin</td>
+            //                 <td>MERN</td>
+            //                 <td>
+            //                     <button className="btn btn-danger btn-sm m=3">Delete</button>&nbsp;
+            //                     <button className="btn btn-primary btn-sm m=3">Edit</button>
+            //                 </td>
+            //             </tr>
+            //             <tr>
+            //                 <td>2</td>
+            //                 <td>Deva</td>
+            //                 <td>CSS</td>
+            //                 <td>
+            //                     <button className="btn btn-danger btn-sm m=3">Delete</button>&nbsp;
+            //                     <button className="btn btn-primary btn-sm m=3">Edit</button>
+            //                 </td>
+            //             </tr>
+            //         </tbody>
+            //     </table>
+            //     <hr />
+            // </div>
+
+        // <div className="container">
+        //     <h2>Search Student</h2>
+        //     <div className="input-group">
+        //         <input type="text" className="form-control" placeholder="Search by Name" />
+        //         <button className="btn btn-primary">Search</button>
+        //     </div>
+        //     <hr />
+        // </div>
+
+        //floating labels
+        // <div className="container">
+        //     <h2>Floating Label</h2>
+        //     <div className="form-floating mb-3">
+        //         <input type="email" className="form-control" id="email" placeholder="Enter Email Address" />
+        //         <label htmlFor="email">Email Address</label>
+        //         <hr />
+        //     </div>
+
+        // </div>
+
+        // <div className="container">
+        //     <h2>Input Group</h2>
+        //     <div className="input-group mb-3">
+        //         <span className="input-group-text">@</span>
+        //         <input type="text" className="form-control" placeholder="Enter Username" />
+        //     </div>
+        // </div>
+    )
+}
+
+export default App;
